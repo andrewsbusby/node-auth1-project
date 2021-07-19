c68644f227993aeba530c41717f6961cfe538cfe
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-07-19 15:12:02 -0600

    update to middleware checking password length

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -65,7 +65,11 @@ async function checkUsernameExists(rew, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  next()
+  if(!req.body.paddword || req.body.password < 3) {
+    next({ message: "Password must be longer than 3 chars", status: 422 })
+  } else {
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
